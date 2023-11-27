df07a3f66c67df3fab884f6a4e674314293d6f01
martinalejandroo25 <martinalejandroo25@gmail.com>
2023-11-27 02:36:38 +0100

    algo de numerales

--- funcionesArrays.java
+++ funcionesArrays.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 public class funcionesArrays {
     ////////////Eliminar un elemento de un array///////////////////////////////
     public static int[] eliminarElemento(int array[], int posicion){
@@ -15,5 +16,16 @@ public class funcionesArrays {
             arraymod[i-1] = array[i];
         }
         return arraymod;
+    }
+
+    /////////Pedir por teclado los elementos de un array en forma de numeral ordinal (No es una función como tal, solo lo coloqué así para mejor visualización)/////////
+    public static void Numeracion(String numeral[]) {
+        Scanner sc = new Scanner(System.in);
+        String posicion[]={"primero", "segundo", "tercer", "cuarto", "quinto","sexto", "septimo", "octavo", "noveno", "decimo"};//////
+        int num[] = new int[10];
+        for (int i = 0; i < num.length; i++) {/////
+            System.out.println("Dame el "+posicion[i]+" numero");
+            num[i]=Integer.parseInt(sc.nextLine());
+        }
     }
 }
